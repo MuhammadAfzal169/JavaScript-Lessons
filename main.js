4e48b0b6340664bf98b8e49ed4f05099a2cf8cd7
Servandelor <muxammadnigmatov1@gmail.com>
2025-10-18 23:11:07 -0400

    Age calculator

---
+++ main.js
@@ -0,0 +1,72 @@
+
+// const currentYear = 2025;
+
+// const lastName = "Smith";
+
+
+// let age = 15;
+// console.log(age);
+// age = 20;
+// console.log(age);
+// age = 21
+// console.log(age);
+
+// Increment by 1
+
+
+
+
+// let x = 1;
+// let y = 2025;
+// let name = "John Doe";
+// console.log(name + " is " + (x + y) + " years old.");
+// console.log();
+
+
+// let statement = true;
+
+// let a = 10;
+// let b = 10;
+
+// console.log(a == b);
+
+// let x;
+// x = 5;
+// console.log(x);
+
+// let z  = null;
+// console.log(z);
+
+// let dreamCar = null;
+// console.log("Right now my dream car is: " + dreamCar );
+// dreamCar = "Porsche";
+// console.log("Right now my dream car is: " + dreamCar );
+
+// const a = Symbol('id');
+// const b = Symbol('id');
+
+// console.log(a === b);
+
+
+let x = BigInt(23456789012345678901234567890);
+
+let checkX = typeof x;
+
+
+
+let a = 10;
+let b = "Apple";
+let c = true;
+let d;
+let e = null;
+let f = Symbol('id');
+let g = BigInt(12345678901234567890);
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
